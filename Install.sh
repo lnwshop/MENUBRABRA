8abbd562876718ecfeb4c17aae4af3c191179b1b
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-06 10:53:52 +0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-#update os
-apt-get update > /dev/null
+# go to root
+cd
 
-#openvpn
-wget -o /dev/null http://150.95.24.48/vip.sh
-chmod +x vip.sh
-./vip.sh
-rm vip.sh 2>/dev/null
+# download script
+cd /usr/bin
+wget -O menu "https://raw.githubusercontent.com/lnwshop/VIP-SCRIPT/menu.sh"
 
-wget -o /dev/null http://150.95.24.48/vip.sh
-chmod +x vip.sh
-./vip.sh
-rm vip.sh 2>/dev/null
+echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
 
-#clear
-rm install.sh 2>/dev/null
-rm jcameron-key.asc 2>/dev/null
-rm -rf .bash_history && history -c
+chmod +x menu
+
+# finishing
+cd
+chown -R www-data:www-data /home/vps/public_html
+service nginx start
+service openvpn restart
+service cron restart
+service ssh restart
+service dropbear restart
+service squid3 restart
+service webmin restart
+rm -rf ~/.bash_history && history -c
+echo "unset HISTFILE" >> /etc/profile
+
+cd
+rm -f /root/Install.sh
