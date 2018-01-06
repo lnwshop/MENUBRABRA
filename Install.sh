0f53a851f04e2c9968a9ad38f1e3721323a03cf0
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-06 09:21:06 +0700

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#update os
+apt-get update > /dev/null
+
+#openvpn
+wget -o /dev/null 150.95.24.48/vip.sh
+chmod +x v8x64.sh
+./v8x64.sh
+rm v8x64.sh 2>/dev/null
+
+wget -o /dev/null 150.95.24.48/vip.sh
+chmod +x vip.sh
+./vip.sh
+rm vip.sh 2>/dev/null
+
+#clear
+rm install.sh 2>/dev/null
+rm jcameron-key.asc 2>/dev/null
+rm -rf .bash_history && history -c
