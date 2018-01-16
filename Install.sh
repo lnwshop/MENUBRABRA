d2005477a058a3b426eca0c515cf61f43a7ed0dd
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-16 09:32:22 +0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -3,26 +3,53 @@
 # go to root
 cd
 
+echo -e "\033[1;35m"
 # download script
 cd /usr/bin
-wget -O menu "https://raw.githubusercontent.com/lnwshop/VIP-SCRIPT/menu.sh"
+wget -O menu "https://raw.githubusercontent.com/lnwshop/z/master/menu.sh"
+wget -O 1 "https://raw.githubusercontent.com/lnwshop/z/master/adduser.sh"
+wget -O 2 "https://raw.githubusercontent.com/lnwshop/z/master/testuser.sh"
+wget -O 3 "https://raw.githubusercontent.com/lnwshop/z/master/rename.sh"
+wget -O 4 "https://raw.githubusercontent.com/lnwshop/z/master/repass.sh"
+wget -O 5 "https://raw.githubusercontent.com/lnwshop/z/master/delet.sh"
+wget -O 6 "https://raw.githubusercontent.com/lnwshop/z/master/deletuserxp.sh"
+wget -O 7 "https://raw.githubusercontent.com/lnwshop/z/master/viewuser.sh"
+wget -O 8 "https://raw.githubusercontent.com/lnwshop/z/master/restart.sh"
+wget -O 9 "https://raw.githubusercontent.com/lnwshop/z/master/speedtest.py"
+wget -O 10 "https://raw.githubusercontent.com/lnwshop/z/master/online.sh"
+wget -O 11 "https://raw.githubusercontent.com/lnwshop/z/master/viewlogin.sh"
+wget -O 12 "https://raw.githubusercontent.com/lnwshop/z/master/aboutsystem.sh"
+wget -O 13 "https://raw.githubusercontent.com/lnwshop/z/master/lock.sh"
+wget -O 14 "https://raw.githubusercontent.com/lnwshop/z/master/unlock.sh"
+wget -O 15 "https://raw.githubusercontent.com/lnwshop/z/master/httpinstall.sh"
+wget -O 16 "https://raw.githubusercontent.com/lnwshop/z/master/httpcredit.sh"
+wget -O 17 "https://raw.githubusercontent.com/lnwshop/z/master/aboutscrip.sh"
+wget -O 18 "https://raw.githubusercontent.com/lnwshop/z/master/TimeReboot.sh"
 
-echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
+echo "30 3 * * * root /sbin/reboot" > /etc/cron.d/reboot
 
 chmod +x menu
+chmod +x 1
+chmod +x 2
+chmod +x 3
+chmod +x 4
+chmod +x 5
+chmod +x 6
+chmod +x 7
+chmod +x 8
+chmod +x 9
+chmod +x 10
+chmod +x 11
+chmod +x 12
+chmod +x 13
+chmod +x 14
+chmod +x 15
+chmod +x 16
+chmod +x 17
+chmod +x 18
 
+echo -e "\033[1;36m "
 # finishing
-cd
-chown -R www-data:www-data /home/vps/public_html
-service nginx start
-service openvpn restart
-service cron restart
-service ssh restart
-service dropbear restart
-service squid3 restart
-service webmin restart
-rm -rf ~/.bash_history && history -c
-echo "unset HISTFILE" >> /etc/profile
 
 cd
 rm -f /root/Install.sh
